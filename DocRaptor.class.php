18df44f0848f23b3fcc39b43970656c3d36683db
Iain Cambridge <wackiebackie@gmail.com>
2011-04-16 12:36:11 +0100

    Added content_url support

--- DocRaptor.class.php
+++ DocRaptor.class.php
@@ -9,6 +9,7 @@ class DocRaptor {
 	
 	protected $api_key;
 	protected $document_content;
+	protected $document_url;
 	protected $document_type;
 	protected $name;
 	protected $test;
@@ -34,6 +35,11 @@ class DocRaptor {
 		return $this;
 	}
 	
+	public function setDocumentUrl($document_url){
+		$this->document_url = $document_url;
+		return $this;
+	}
+	
 	public function setDocumentType($document_type){
 		$document_type = strtolower($document_type);
 		$this->type = $document_type == 'pdf' || $document_type == 'xls' ? $document_type : 'pdf';
@@ -54,11 +60,16 @@ class DocRaptor {
 		if($this->api_key != ''){
 			$url = "https://docraptor.com/docs?user_credentials=".$this->api_key;
 			$fields = array(
-				'doc[document_content]'=>urlencode($this->document_content),
 				'doc[document_type]'=>$this->type,
 				'doc[name]'=>$this->name,
 				'doc[test]'=>$this->test
 			);
+			if ( !empty($this->document_content) ){
+				$fields['doc[document_content]'] = urlencode($this->document_content);
+			} else {
+				$fields['doc[document_url]'] = urlencode($this->document_url);
+			}
+			
 			foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; } 
 			rtrim($fields_string,'&');
 			$ch = curl_init();
