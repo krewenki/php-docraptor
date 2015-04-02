18ccca5af78005098a4c1dac2ce047e1d9e39dd0
Markus <markus.hausammann@cloud-solutions.net>
2015-04-03 00:20:34 +0300

    fixed wrong status code

--- src/Exception/UnprocessableEntityException.php
+++ src/Exception/UnprocessableEntityException.php
@@ -5,6 +5,6 @@ class UnprocessableEntityException extends RuntimeException
 {
     public function __construct()
     {
-        parent::__construct('Your document contains syntax errors and DocRaptor cannot process it.', 433);
+        parent::__construct('Your document contains syntax errors and DocRaptor cannot process it.', 422);
     }
 }
